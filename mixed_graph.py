32621d4323a513f9f63424090112db16941e70e0
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2016-03-21 21:48:49 +0100

    Fix bugs in constructor, preserve vertex positions if applicable

--- mixed_graph.py
+++ mixed_graph.py
@@ -1,4 +1,5 @@
 from sage.graphs.digraph import DiGraph
+from sage.graphs.generic_graph import GenericGraph
 from sage.graphs.graph import Graph
 from sage.plot.arrow import Arrow, CurveArrow
 from sage.plot.bezier_path import BezierPath
@@ -25,19 +26,20 @@ class MixedGraph(DiGraph):
                 self._arcs = data._arcs
                 init = False
             elif isinstance(data, Graph):
-                if edges is not None or arcs is not None:
+                if edges is not None:
                     raise ValueError("Edges should not be specified with a Graph")
                 edges = data.edges(labels=False)
                 if arcs is None:
                     arcs = []
             elif isinstance(data, DiGraph):
-                if edges is not None or arcs is not None:
+                if arcs is not None:
                     raise ValueError("Arcs should not be specified with a DiGraph")
-                arcs = data.arcs(labels=False)
+                arcs = data.edges(labels=False)
                 if edges is None:
                     edges = []
             elif arcs is not None and edges is None:
                 edges = data
+                data = None
             else:
                 if edges is not None or arcs is not None:
                     raise ValueError("Edges or arcs should not be specified with other data")
@@ -56,6 +58,9 @@ class MixedGraph(DiGraph):
             self._arcs = [(u, v, i+n) for i, (u, v) in enumerate(arcs)]
         DiGraph.__init__(self, self._edges + self._arcs,
                          multiedges = multiedges, loops = loops, **kargs)
+        if isinstance(data, GenericGraph) and data._pos is not None and \
+            kargs.setdefault('pos', None) is None and len(data) == len(self):
+                self._pos = data._pos
 
     def graphplot(self, **options):
         options['edge_colors'] = {EDGE_COLOR: self._edges,
